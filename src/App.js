c29cf230a5476a3e3d88b9a65f616ca7474bc730
rainof <13416303+rainof@users.noreply.github.com>
2024-07-30 19:09:58 +0700

    Set up initial project by adjusting the structure of text and image

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.scss';
+import product_img from './images/image-product-mobile.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <div className="product">
+        <img className="product-img" src={product_img} alt="product" />
+        <div className="product-label">Perfume</div>
+        <div className="product-name">Gabrielle Essence Eau De Parfum</div>
+        <div className="product-detail">A floral, solar and voluptuous interpretation composed by Olivier Polge,
+      Perfumer-Creator for the House of CHANEL.</div>
+      </div>
+      <div className="payment">
+        <div className="discount-cost">$149.99</div>
+        <div className="actual-cost">$169.99</div>
+        <button className="btn btn-primary">Add to Cart</button>
+      </div>
     </div>
   );
 }
